4708c8ad13b581b51f15906481c8d63efdb52d38
akio1230 <huangxinqiuwei@gmail.com>
2026-05-23 18:02:01 +0900

    1

---
+++ kadai2/kadai2/kadai2.cpp
@@ -0,0 +1,16 @@
+﻿// kadai2.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int a;
+    int b;
+    char c;
+
+    printf("数字を2つと演算子をa + b このように入力してください\n");
+    
+}
+

c1a2b9231c5abf8c53ba591f92ecc2163eca95a8
akio1230 <huangxinqiuwei@gmail.com>
2026-05-26 11:03:12 +0900

    電卓プログラム

--- kadai2/kadai2/kadai2.cpp
+++ kadai2/kadai2/kadai2.cpp
@@ -2,15 +2,59 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main()
 {
-    int a;
-    int b;
-    char c;
+	int num;
+	int num1;
+	char ope;
+	char input;
 
-    printf("数字を2つと演算子をa + b このように入力してください\n");
-    
+	while (1)
+	{
+		printf("演算子 (+, -, *, /) を入力してください\n");
+		scanf_s("%c", &ope);
+
+		printf("数値を1個ずつ入力してください\n");
+		scanf_s("%d  %d", &num, &num1);
+
+		switch (ope)
+		{
+		case '+':
+			printf("足し算の結果は %d です\n", num + num1);
+			break;
+		case '-':
+			printf("引き算の結果は %d です\n", num - num1);
+			break;
+		case '*':
+			printf("掛け算の結果は %d です\n", num * num1);
+			break;
+		case '/':
+			if (num1 == 0.0)
+			{
+				printf("ゼロでの割り算はできません\n");
+				
+			}
+			printf("割り算の結果は %d です\n", num / num1);
+			break;
+		}
+		printf("---------------------------------\n");
+
+		printf("計算を続けますか？続けるのならyを押してください、終わるのならyいがいを押してください\n");
+
+		scanf_s("%c", &input);
+
+		if (input != 'y' && input != 'Y')
+		{
+			break;
+		}
+
+		printf("----------------------------------\n");
+
+	}
+	return 0;
+
 }
 
